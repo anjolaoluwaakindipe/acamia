78a73da326cb70879a755443edd948080dd20b42
anjolaoluwaakindipe <anjyakindipe@gmail.com>
2023-12-26 13:21:13 -0500

    Development: added registry test 3

--- lib/backend/domain/entities/src/lib/department.entity.ts
+++ lib/backend/domain/entities/src/lib/department.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  Index,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Course } from './course.entity';
 
 @Entity()
